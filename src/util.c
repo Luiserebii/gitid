91011dea3bb8fd419f48a1a6d099dae1819d3fe2
Luiserebii <luis@serebii.io>
2020-01-25 03:43:14 -0500

    Attempting to sketch out util functions

--- src/util.c
+++ src/util.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * Escapes any ' by replacing with '\'', wrapping the entire
@@ -10,12 +11,32 @@
  * behavior can arise.
  */
 void escapesh(char* str) {
-    
+    //Declare and load buffer with copy of str
+    char buffer[strlen(str) + 1];
+    strcpy(buffer, str);
+
+    //Write escaped values to str
+    for(char* it = buffer; *it; ++it, ++str) {
+        if(*it == ''') {
+            //Write '\''
+
+        }
+    }
+
 }
 
 /**
- * A wrapped call of the stdlib int system() function.
+ * A wrapped call of the stdlib int system() function. Prints any error
+ * to the standard error stream and exits.
+ *
+ * Returns the exit code of the command run if successful.
  */
-void minsystem(const char* str) {
-    //
+int minsystem(const char* str) {
+    int code;
+    if((code = system(str)) != -1) {
+        return code;
+    } else {
+        fprintf("Internal error: Failure to execute \"%s\"\n", str);
+        exit(1);
+    }
 }
