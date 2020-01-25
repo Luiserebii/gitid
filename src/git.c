069760af3704286fe50bb72a8d2b938da292ee51
Luiserebii <luis@serebii.io>
2020-01-25 05:21:07 -0500

    More formed interface written in git.c

--- src/git.c
+++ src/git.c
@@ -3,21 +3,39 @@
 
 #include <stdlib.h>
 
-#define MAXLINE 1000
+#define GIT_USER_MAXSTRING 1000
 
 /**
- *
+ * Allocates space for a new git_user struct.
  */
 void git_user_init(git_user* user) {
     //Allocate for struct
     user = malloc(sizeof(git_user));
 
     //Allocate each string
-    user->name = malloc(sizeof(char))
+    user->name = malloc(sizeof(char) * GIT_USER_MAXSTRING);
+    user->email = malloc(sizeof(char) * GIT_USER_MAXSTRING);
+    user->signing_key = malloc(sizeof(char) * GIT_USER_MAXSTRING);
 }
 
+/**
+ * Obtains the global git_user condif and saves it into the 
+ * git_user struct passed.
+ */
 void git_get_user_global(git_user* user) {
-    runcmd("git config --global user.name", MAXLINE, user->name);
-    runcmd("git config --global user.email", MAXLINE, user->email);
-    runcmd("git config --global user.signing_key", MAXLINE, user->signing_key);
+    runcmd("git config --global user.name", GIT_USER_MAXSTRING, user->name);
+    runcmd("git config --global user.email", GIT_USER_MAXSTRING, user->email);
+    runcmd("git config --global user.signing_key", GIT_USER_MAXSTRING, user->signing_key);
+}
+
+/**
+ * Frees git_user struct.
+ */
+void git_user_free(git_user* user) {
+    //Free members
+    free(git_user->name);
+    free(git_user->email);
+    free(git_user->signing_key);
+    //Free struct
+    free(user);
 }
