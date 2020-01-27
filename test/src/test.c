2dfde050e39f568e2a7e597a1c2d0f6e55112051
Luiserebii <luis@serebii.io>
2020-01-26 23:33:18 -0500

    Decided to test write functions by writing to a temporary directory, which can be manually checked afterwards, much better way of doing things I think. Only one issue; we're segfaulting (after running through GDB, it seems like we're getting NULL from fopen(), as it is failing)

--- test/src/test.c
+++ test/src/test.c
@@ -7,9 +7,9 @@
 #include <stdio.h>
 #include <string.h>
 
-void test_git_user();
 void test_git_user_init();
 void test_git_user_set_functions();
+void test_git_user_write();
 void test_escapesh();
 void test_runcmd();
 void test_trimNewline();
@@ -20,9 +20,9 @@ void tearDown() {}
 int main() {
 
     UNITY_BEGIN();
-    RUN_TEST(test_git_user);
     RUN_TEST(test_git_user_init);
     RUN_TEST(test_git_user_set_functions);
+    RUN_TEST(test_git_user_write);
     
     RUN_TEST(test_escapesh);
     RUN_TEST(test_runcmd);
@@ -30,27 +30,18 @@ int main() {
     return UNITY_END();
 }
 
-void test_git_user() {
-    git_user* user = git_user_init();
-    git_get_user_global(user);
+void test_git_user_write() {
+    git_user* id = git_user_init();
+    
+    git_user_set_name(id, "Luiserebii");
+    git_user_set_email(id, "luis@serebii.io");
+    git_user_set_signing_key(id, "3B7E2D68E27CBBCF");
 
-    printf("User obtained:\n name: %s | email: %s | signingKey: %s\n", user->name, user->email, user->signing_key);
-
-    git_user_free(user);
-
-    gitid_id* id = gitid_id_init();
-    gitid_id_set_id_name(id, "Luiserebii");
-    gitid_id_set_name(id, "Luiserebii");
-    gitid_id_set_email(id, "luis@serebii.io");
-    gitid_id_set_signing_key(id, "SIGNING_KEY");
-
-    gitid_id_write(id, stdout);
-    gitid_id_min_write(id, stdout);
-    //Attempt read
-    //gitid_id_min_read(id, stdin);
-    //Print read
-    //gitid_id_min_write(id, stdout);
-    TEST_ASSERT(1);
+    //Temporary file to write to
+    FILE* tmp = fopen("../tmp/temp_test_git_user_write", "w");
+    git_user_write(id, tmp);
+    int res = fclose(tmp);
+    TEST_ASSERT(res != EOF)
 }
 
 void test_git_user_set_functions() {
