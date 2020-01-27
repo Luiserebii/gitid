0e93e01630ab64a0362b100c4e364df68e5f3946
Luiserebii <luis@serebii.io>
2020-01-26 20:06:28 -0500

    Moving primitive tests out

--- src/git.c
+++ src/git.c
@@ -158,13 +158,3 @@ void git_clone(git_clone_opts* opts) {
     //Finally, execute constructed command
     minsystem(cmd);
 }
-
-int main() {
-
-    git_user* user = git_user_init();
-    git_get_user_global(user);
-
-    printf("User obtained:\n name: %s | email: %s | signingKey: %s\n", user->name, user->email, user->signing_key);
-
-    git_user_free(user);
-}
---
+++ test/src/test.c
@@ -0,0 +1,15 @@
+#include "../../include/git.h"
+#include "../../include/git-user.h"
+
+#include <stdio.h>
+
+int main() {
+
+    git_user* user = git_user_init();
+    git_get_user_global(user);
+
+    printf("User obtained:\n name: %s | email: %s | signingKey: %s\n", user->name, user->email, user->signing_key);
+
+    git_user_free(user);
+}
+
