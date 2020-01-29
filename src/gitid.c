2bec92f5af2101827739d38d56b56470022f70e5
Luiserebii <luis@serebii.io>
2020-01-29 05:30:22 -0500

    Implemented gitid_set_system_gitid_ids... was fairly simple with our abstractions. However, not entirely sure how to test this... is there a way to shift the GITID_SYSTEM_DATA_FILE macro for testing, or should I maybe create a sub-function that takes a filename, make these functions macros, and then test the sub-functions...?

--- src/gitid.c
+++ src/gitid.c
@@ -1,6 +1,6 @@
 #include "../include/gitid.h"
-#include "../include/vector-gitid-id.h"
 #include "../include/gitid-id.h"
+#include "../include/vector-gitid-id.h"
 
 void gitid_get_system_gitid_ids(vector_gitid_id* v) {
     //Declare gitid_id for use
@@ -8,7 +8,7 @@ void gitid_get_system_gitid_ids(vector_gitid_id* v) {
 
     //Open file stream to system file
     FILE* sys_gitids = fopen(GITID_SYSTEM_DATA_FILE, "r");
-    
+
     int c;
     while((c = fgetc(sys_gitids)) != EOF) {
         //Attempt ungetc, now that we know we haven't
@@ -22,9 +22,20 @@ void gitid_get_system_gitid_ids(vector_gitid_id* v) {
         //Finally, push onto vector
         vector_push_back_gitid_id(v, id);
     }
+
+    //Close stream
+    fclose(sys_gitids);
 }
-/*
+
 void gitid_set_system_gitid_ids(vector_gitid_id* v) {
+    //Open file stream to system file in write mode
+    FILE* sys_gitids = fopen(GITID_SYSTEM_DATA_FILE, "w");
 
-}*/
+    //Iterate through vector and write each one
+    for(gitid_id** it = v->head; it != v->avail; ++it) {
+        gitid_id_min_write(*it, sys_gitids);
+    }
 
+    //Close stream
+    fclose(sys_gitids);
+}
