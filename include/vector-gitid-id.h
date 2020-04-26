0a8b5aa02f017e4dab1d38be237437fc3d04ef00
Luiserebii <luis@serebii.io>
2020-04-26 04:01:11 -0400

    Attempting to move vector-gitid-id to this new stack architecture, and refactoring along the way by expanding define_vector_type

--- include/vector-gitid-id.h
+++ include/vector-gitid-id.h
@@ -2,111 +2,22 @@
 #define VECTOR_GITID_ID_H
 
 #include "gitid-id.h"
+#include <cstl/vector.h>
 
-typedef struct {
-    gitid_id** head;
-    gitid_id** avail;
-    gitid_id** tail;
-} vector_gitid_id;
-
-/*
- * Initializes an empty vector within the vector* passed.
- *
- * Note that although this data structure is completely empty, and has
- * no elements allocated, the struct itself is, so vector_free is necessary
- * for cleanup afterwards.
- */
-vector_gitid_id* vector_init_gitid_id(void);
-
-/*
- * Initializes an empty vector with the size passed.
- * Memory will be allocated under the size given, so vector_free
- * is required.
- */
-vector_gitid_id* vector_init_size_gitid_id(size_t s);
-
-/*
- * Intiailizes an empty vector with the size passed.
- * Unlike vector_init_size, this function will fill the capacity.
- */
-vector_gitid_id* vector_init_capacity_gitid_id(size_t s);
-/**
- * This function is a little complex at the moment, and probably not immediately
- * needed, so let's implement this down the line if necessary
- */
-//vector_gitid_id* vector_copy_gitid_id(vector_gitid_id* src);
-
-/*
- * Sets an element of the vector to the value passed.
- */
-void vector_set_gitid_id(vector_gitid_id* v, size_t pos, gitid_id* ptr);
-//Frees the gitid_id at the position, if any, before setting
-void vector_set_free_gitid_id(vector_gitid_id* v, size_t pos, gitid_id* ptr);
-
-/*
- * Pushes a new element onto the vector.
- */
-void vector_push_back_gitid_id(vector_gitid_id* v, gitid_id* e);
-
-/*
- * Erases an element at the pointer passed from the vector. Returns a pointer to the
- * new end of the vector.
- */
-gitid_id** vector_erase_gitid_id(vector_gitid_id* v, gitid_id** pos);
-
-/*
- * Erases a range of elements [begin, end) from the vector. Returns a pointer to the
- * new end of the vector.
- */
-gitid_id** vector_erase_range_gitid_id(vector_gitid_id* v, gitid_id** begin, gitid_id** end);
-gitid_id** vector_erase_free_gitid_id(vector_gitid_id* v, gitid_id** pos);
-
-/*
- * Returns the first pointer in the vector's sequence.
- */
-gitid_id** vector_begin_gitid_id(vector_gitid_id* v);
-
-/*
- * Returns the last pointer in the vector's sequence.
- */
-gitid_id** vector_end_gitid_id(vector_gitid_id* v);
-
-/*
- * Returns the current size of the vector.
- */
-size_t vector_size_gitid_id(vector_gitid_id* v);
-
-/*
- * Returns the current total capacity of the vector,
- * which encompasses the total amount allocated.
- */
-size_t vector_capacity_gitid_id(vector_gitid_id* v);
-
-/*
- * Returns the element found at the specified location passed.
- */
-gitid_id* vector_at_gitid_id(vector_gitid_id* v, size_t n);
+declare_vector_type(gitid_id);
 
 /**
  * Returns a pointer to a gitid_id by searching for the first with the name passed.
  * If nothing was found, NULL is returned.
  */
-gitid_id* vector_get_id_gitid_id(vector_gitid_id* v, const char* id_name);
+gitid_id* vector_gitid_id_get_id(vector_gitid_id* v, const char* id_name);
 
 /**
- * 
- * Clears all elements allocated to the vector, but does not deallocate
- * the vector itself.
- * Clear will free the vector of the elements, but not the elements themselves.
+ * Frees the resources allocated to all vector elements through a kind of recursive
+ * deinit.
  *
- * Dangerous function to call, as you can lose references to elements which
- * cannot be deallocated afterwards.
+ * deinit_r frees the vector's elements of all resources, but not the vector itself.
  */
-void vector_clear_gitid_id(vector_gitid_id* v);
-/**
- * Frees the resources allocated to the vector, including the vector itself.
- * Free frees the vector of all resources, including the elements.
- */
-void vector_free_gitid_id(vector_gitid_id* v);
+void vector_gitid_id_deinit_r(vector_gitid_id* v);
 
 #endif
--- src/vector-gitid-id.c
+++ src/vector-gitid-id.c
@@ -2,132 +2,25 @@
 #include "../include/gitid-id.h"
 #include "../include/util.h"
 
+#include <cstl/vector.h>
 #include <stdlib.h>
 #include <string.h>
 
-static void vector_grow_gitid_id(vector_gitid_id* v);
+define_vector_type(gitid_id);
 
-vector_gitid_id* vector_init_gitid_id(void) {
-    vector_gitid_id* v = (vector_gitid_id*) safemalloc(sizeof(vector_gitid_id));
-    v->head = v->avail = v->tail = NULL;
-    return v;
-}
-
-vector_gitid_id* vector_init_size_gitid_id(size_t s) {
-    vector_gitid_id* v = (vector_gitid_id*) safemalloc(sizeof(vector_gitid_id));
-    v->head = (gitid_id**) safemalloc(sizeof(gitid_id*) * s);
-    v->tail = v->avail = v->head + s;
-    return v;
-}
-
-vector_gitid_id* vector_init_capacity_gitid_id(size_t s) {
-    vector_gitid_id* v = (vector_gitid_id*) safemalloc(sizeof(vector_gitid_id));
-    v->head = v->avail = (gitid_id**) safemalloc(sizeof(gitid_id*) * s);
-    v->tail = v->head + s;
-    return v;
-}
-/*
-vector_gitid_id* vector_copy_gitid_id(vector_gitid_id* src) {
-    if(src->head == NULL) {
-        return vector_init_gitid_id();
-    }
-    vector_gitid_id* copy = vector_init_size_gitid_id(vector_size_gitid_id(src));
-    //algorithm_min_copy(vector_type*, src->head, src->avail, copy->head);
-    {
-        const gitid_id* _alg_copy_it = src->head;
-        for(gitid_id* _alg_copy_dest = copy->head; _alg_copy_it != src->avail; *_alg_copy_dest++ = *_alg_copy_it++)
-            ;
-    };
-    return copy;
-}
-*/
-void vector_set_gitid_id(vector_gitid_id* v, size_t pos, gitid_id* ptr) {
-    v->head[pos] = ptr;
-}
-
-void vector_set_free_gitid_id(vector_gitid_id* v, size_t pos, gitid_id* ptr) {
-    gitid_id_free(v->head[pos]);
-    v->head[pos] = ptr;
-}
-
-void vector_push_back_gitid_id(vector_gitid_id* v, gitid_id* e) {
-    if(v->avail == v->tail) {
-        vector_grow_gitid_id(v);
-    }
-    *(v->avail++) = e;
-}
-
-gitid_id** vector_erase_gitid_id(vector_gitid_id* v, gitid_id** pos) {
-    for(; pos + 1 != v->avail; ++pos) {
-        *pos = *(pos + 1);
-    }
-    return --v->avail;
-}
-
-gitid_id** vector_erase_range_gitid_id(vector_gitid_id* v, gitid_id** begin, gitid_id** end) {
-    size_t diff = end - begin;
-    for(; begin + diff != v->avail; ++begin) {
-        *begin = *(begin + diff);
-    }
-    return v->avail -= diff;
-}
-
-gitid_id** vector_erase_free_gitid_id(vector_gitid_id* v, gitid_id** pos) {
-    gitid_id_free(*pos);
-    for(; pos + 1 != v->avail; ++pos) {
-        *pos = *(pos + 1);
-    }
-    return --v->avail;
-}
-
-gitid_id** vector_begin_gitid_id(vector_gitid_id* v) {
-    return v->head;
-}
-
-gitid_id** vector_end_gitid_id(vector_gitid_id* v) {
-    return v->avail;
-}
-
-void vector_grow_gitid_id(vector_gitid_id* v) {
-    size_t old_size = vector_size_gitid_id(v);
-    size_t n_size = v->head ? old_size * 2 : 1;
-    v->head = (gitid_id**) saferealloc(v->head, sizeof(gitid_id*) * n_size);
-    v->avail = v->head + old_size;
-    v->tail = v->head + n_size;
-}
-
-size_t vector_size_gitid_id(vector_gitid_id* v) {
-    return v->avail - v->head;
-}
-
-size_t vector_capacity_gitid_id(vector_gitid_id* v) {
-    return v->tail - v->head;
-}
-
-gitid_id* vector_at_gitid_id(vector_gitid_id* v, size_t n) {
-    return *(v->head + n);
-}
-
-gitid_id* vector_get_id_gitid_id(vector_gitid_id* v, const char* id_name) {
-    for(gitid_id** it = v->head; it != v->avail; ++it) {
-        if(string_cmp_cstr((*it)->id_name, id_name) == 0) {
-            return *it;
+gitid_id* vector_gitid_id_get_id(vector_gitid_id* v, const char* id_name) {
+    for(gitid_id* it = v->head; it != v->avail; ++it) {
+        if(string_cmp_cstr(&it->id_name, id_name) == 0) {
+            return it;
         }
     }
     //If we hit here, nothing was found
     return NULL;
 }
 
-void vector_clear_gitid_id(vector_gitid_id* v) {
-    free(v->head);
-    v->head = v->avail = v->tail = NULL;
-}
-
-void vector_free_gitid_id(vector_gitid_id* v) {
-    //Free all gitid_ids
-    for(gitid_id** it = v->head; it != v->avail; ++it) {
-        gitid_id_free(*it);
+void vector_gitid_id_deinit_r(vector_gitid_id* v) {
+    //Deinitializes all gitid_ids
+    for(gitid_id* it = v->head; it != v->avail; ++it) {
+        gitid_id_deinit(it);
     }
-    free(v->head);
-    free(v);
 }
