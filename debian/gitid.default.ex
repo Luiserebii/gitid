b003a6be982f84315532225ed60d228b6d981dd3
Luiserebii <luis@serebii.io>
2020-05-12 21:01:25 -0400

    Comitting initial debian generated subdirectory

---
+++ debian/gitid.default.ex
@@ -0,0 +1,10 @@
+# Defaults for gitid initscript
+# sourced by /etc/init.d/gitid
+# installed at /etc/default/gitid by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(gitid):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="gitid" command="/usr/bin/gitid"
